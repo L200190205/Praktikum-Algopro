7ae2d7250e412fd746cb6adcaa5badb675e9262e
L200190205 <57501111+L200190205@users.noreply.github.com>
2019-12-07 12:33:41 +0700

    Update Kegiatan 3 Server.py

--- Praktikum 10/Kegiatan 3 Server.py
+++ Praktikum 10/Kegiatan 3 Server.py
@@ -17,17 +17,17 @@ while perintah != 'keluar':
             break
         print "pesan:",perintah
         if len(item)==2:
-            if perintah == 'alas':
+            if perintah == 'Luas alas':
                 a=int(item[1])
-                komm,send('alas disimpan')
-            elif perintah =='tinggi':
+                komm,send('Luas alas disimpan')
+            elif perintah =='Luas sisi tegak':
                 t=int(item[1])
-                komm.send('tinggi disimpan')
+                komm.send('Luas sisi tegak disimpan')
             else:
                 komm.send('Pesan tidak diketahui')
         elif perintah == 'hitung':
-            L=float(a*t)
-            response = 'Luas jajar genjang dengan alas {} tinggi {} adalah {}'.format(a,t,L)
+            L=float(a+4*t)
+            response = 'Luas Piramid dengan Luas alas {} Luas sisi tegak {} adalah {}'.format(a,t,)
             komm.send(response)
         else:
             komm.send('Pesan tidak diketahui')
